196f89517643d9ee502ceabe5f4d6de60a35e168
bala-amypo <bala@amypo.in>
2025-12-22 08:17:10 +0000

    User commit

---
+++ src/main/java/com/example/demo/config/OpenAPIConfig.java
@@ -0,0 +1,23 @@
+package com.example.demo.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+    info = @Info(title = "Cold Chain API", version = "v1"),
+    security = @SecurityRequirement(name = "bearerAuth") // Apply to all paths
+)
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT"
+)
+public class OpenAPIConfig {
+    // This class enables the "Authorize" button in Swagger UI
+}
